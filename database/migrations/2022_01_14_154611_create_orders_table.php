c9b025063cad8b074e89c66c3eeae630fbcfe086
aziyan99 <rajaazian08@gmail.com>
2022-01-19 19:27:16 +0700

    order migration

--- database/migrations/2022_01_14_154611_create_orders_table.php
+++ database/migrations/2022_01_14_154611_create_orders_table.php
@@ -15,6 +15,10 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string('number');
+            $table->decimal('total_price', 10, 2);
+            $table->enum('payment_status', ['1', '2', '3', '4'])->comment('1=menunggu, 2=selesai, 3=kadaluarsa, 4=batal');
+            $table->string('snap_token')->nullable();
             $table->timestamps();
         });
     }
